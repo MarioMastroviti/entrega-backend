e71d9a8ca66a6278d0e519d3679a4ff8b951d5fc
Mario Mastroviti <mariomastroviti@hotmail.com>
2023-07-31 23:00:04 -0300

    entrega del desafio

---
+++ app.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const express =  require('express')
+const app = express();
+
+const contenedor = require("./contenedor");
+
+
+const port = 8080
+
+
+app.get("/api/productos", async(req,res) => {
+    const todosProductos = await contenedor.obtenerTodosObjetos()
+    res.json(todosProductos)
+});
+
+app.get("/api/productoRandom", async(req,res) => {
+    const todosProductos = await contenedor.obtenerTodosObjetos()
+    const maxId = todosProductos.length;
+
+    const randomNum = generateRandom(1, maxId)
+    const randomProduct = await contenedor.obtenerPorId(randomNum)
+    res.send(randomProduct)
+});
+
+
+const generateRandom = (min, max) => {
+    return Math.floor((Math.random() * (max+1 -min)) + min)
+}
+
+app.listen(port , () => {
+    console.log('Escuchando el puerto 8080')
+});
+
